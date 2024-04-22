1405a7b2bbf1ad407fd4c95e4103084449bb8ebf
Johann Senoner <144158990+GalacticSage@users.noreply.github.com>
2024-04-22 11:56:17 +0200

    add project

---
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotel;
+
+/**
+ *
+ * @author INF3A_usenj
+ */
+public class Hotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
