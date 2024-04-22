d5d9ea784cb628f9e2fe7a2ed6cc89c48ab932d6
Johann Senoner <senoner.johann@proton.me>
2024-04-22 12:39:31 +0200

    Added city

---
+++ src/main/java/com/mycompany/hotel/city.java
@@ -0,0 +1,90 @@
+package com.mycompany.hotel;
+
+import java.sql.*;
+
+public class city {
+    private Connection conn;
+    private Statement statement;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    private int city_id;
+    private String city_name;
+    private String country_abr;
+
+    public city() throws SQLException {
+        this.conn = DBSingleton.getInstance().getConnection();
+        this.statement = conn.createStatement();
+    }
+
+    public city(int city_id) throws SQLException {
+        this.conn = DBSingleton.getInstance().getConnection();
+        this.statement = conn.createStatement();
+
+        this.city_id = city_id;
+        readName();
+    }
+
+    public city(String city_name) throws SQLException {
+        this.conn = DBSingleton.getInstance().getConnection();
+        this.statement = conn.createStatement();
+
+        this.city_name = city_name;
+        readId();
+    }
+
+    public city(int city_id, String city_name, String country_abr) throws SQLException {
+        this.city_id = city_id;
+        this.city_name = city_name;
+        this.country_abr = country_abr;
+
+        this.conn = DBSingleton.getInstance().getConnection();
+        this.statement = conn.createStatement();
+    }
+
+    public void readName() throws SQLException {
+        resultSet = statement.executeQuery("SELECT city_name FROM city WHERE city_id = " + city_id);
+        while (resultSet.next()) {
+            city_name = resultSet.getString("city_name");
+        }
+        this.city_name = city_name;
+    }
+
+    public void readId() throws SQLException {
+        resultSet = statement.executeQuery("SELECT city_id FROM city WHERE city_name = '" + city_name + "'");
+        while (resultSet.next()) {
+            city_id = resultSet.getInt("city_id");
+        }
+    }
+
+    public void insert() throws SQLException {
+        preparedStatement = conn.prepareStatement("INSERT INTO city (city_name, country_abr) VALUES (?, ?)");
+        preparedStatement.setString(1, city_name);
+        preparedStatement.setString(2, country_abr);
+        preparedStatement.executeUpdate();
+    }
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
+
+    public String getCountry_abr() {
+        return country_abr;
+    }
+
+    public void setCountry_abr(String country_abr) {
+        this.country_abr = country_abr;
+    }
+}
